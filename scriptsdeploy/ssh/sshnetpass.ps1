f9a6dd6e6e7ac347de25f79d97e9c0a2fb557122
s364518 <133647120+s364518@users.noreply.github.com>
2024-05-29 13:48:11 +0200

    Update sshnetpass.ps1

--- scriptsdeploy/ssh/sshnetpass.ps1
+++ scriptsdeploy/ssh/sshnetpass.ps1
@@ -1,6 +1,17 @@
 # Slått sammen ssh.ps1 og netpass.ps1 så nettverkspassord også sendes
 # Har ikke testet dette scriptet med sending av passord
 
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType Automatic
+
+cd C:\ProgramData\ssh
+New-Item -ItemType File -Path ".\administrators_authorized_keys"
+icacls.exe "C:/ProgramData/ssh/administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+Add-Content -Path ".\administrators_authorized_keys" -Value "
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwAFU06iFUe3eKSgT+l1P7gS0i86mkPYXt4H/1l8qfkQWSek1pW15v7SRM9Chdpdu4CG5ygTkOzywr+skVdW3UuSeLdCWenGt9r8AF3mPI+J2AucZeJW0o5qUKEmLk3+X7n69G1rZzbPhxkzNjdP8eptQ8RBDH7S0Ef5cPjCu0Ci9sEGuDXi0aINuo7Kah6QvKUQqF0ZS5DkZ3WJhMvcg/z2l84hPfg3DaulZ4yA+c8EvyxGOnLcgYC51DRRo2KZSQuAfsSYL83VlGDIxIegjFlgSrWj+PVo6e16135jT2nbOVvlN/Jhz8bPA8PpmHdamjoNr1qUtJnHIUYMBezNxDYmwlDJuTOoG0lyZCZPF+9GXIyHqSDA4192Cq5u4kWP1CiJ77/0UbfNJmXi5+n3E/CBjQuiIPwh8fpS03xPpYpafLPEWO1tUfjIdruApdiS9ljayDLg76Cv8ZBCoScIxnr3d0ava0BJF4wIFyhcRZYBjwtKvSShcMVw7hFQxJj48=
+"
+
 # Define the URL for the Firestore database
 $projectID = "bachelor-7e242"  # Your project ID
 $collectionName = "machines"  # Target collection
